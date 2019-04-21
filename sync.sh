ef4cc81d9ab6cc5434095d10ea6e6a002e8c5612
Shenghe Bi <bishenghe@126.com>
2019-04-21 13:52:15 +1000

    finish tutorial 2 on ubuntu

--- sync.sh
+++ sync.sh
@@ -1,2 +1,7 @@
 # rsync -avz --delete /Users/Eli/dev/devops/docker-centos root@192.168.1.110:/root/
-rsync -avz --delete /Users/Eli/dev/devops/docker-ubuntu root@192.168.1.110:/root/
+# rsync -avz --delete /Users/Eli/dev/devops/docker-ubuntu root@192.168.1.110:/root/
+
+ANSIBLE_CR="192.168.0.118"
+
+cat ~/.ssh/id_rsa.pub | ssh "root@${ANSIBLE_CR}" "mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys"
+rsync -avz --delete /Users/Eli/dev/devops/docker-ubuntu "root@${ANSIBLE_CR}:/root/"
